8b436bdf0e66e48de38688ab018ffc9f3630468a
Kasymzhan Abdyldayev <kasymzhan.abdyldayev@gmail.com>
2021-07-28 08:30:29 +0900

    First commit

---
+++ app/src/main/java/com/example/resistance/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.resistance;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        Button playButton = findViewById(R.id.playButton);
+//        playButton.callOnClick();
+    }
+}
